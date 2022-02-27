ea907b1bff8e28bc24d92d0e353646293e1e4fd0
Rathkalyforgna CHHIN <rathkalyforgna@gmail.com>
2022-02-27 09:53:11 +0700

    Add route demo

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello, server!');
+app.get("/", (req, res) => {
+  res.send("Hello, server!");
+});
+
+app.get("/demo", (req, res) => {
+  res.set("X-Full-Stack", "4life");
+  res.status(418);
+  res.send("I am teapot");
 });
 
 app.listen(port, () => {
-	console.log(`App listening on port: ${port}`)
+  console.log(`App listening on port: ${port}`);
 });
-
